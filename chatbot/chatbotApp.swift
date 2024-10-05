6771bd6a6c553696e05a1150cb21b4b95ccfa56f
Giannis Hatziioannidis <jhatzics@gmail.com>
2024-10-05 09:15:02 +0300

    Initial Commit

---
+++ chatbot/chatbotApp.swift
@@ -0,0 +1,17 @@
+//
+//  chatbotApp.swift
+//  chatbot
+//
+//  Created by Giannis Hatziioannidis on 5/10/24.
+//
+
+import SwiftUI
+
+@main
+struct chatbotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
